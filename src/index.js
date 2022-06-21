889faa6eb4b196422981d66c94a3013deedd10d5
MES64 <markskelton10@gmail.com>
2022-06-21 23:47:42 +0100

    Static Version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,137 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+
+// Interesting Components
+
+class Carousel extends React.Component {
+  render () {
+    return(
+      <h2>Carousel</h2>
+    );
+  }
+}
+
+class ContactForm extends React.Component {
+  render () {
+    return(
+      <p>Contact Form</p>
+    );
+  }
+}
+
+class ContactUsBody extends React.Component {
+  render () {
+    return(
+      <ContactForm />
+      // <p>Submission Message</p>
+    );
+  }
+}
+
+// Pages
+
+class HomePg extends React.Component {
+  render() {
+    return(
+      <div>
+        <Carousel />
+        <div>
+          <p>Learn More</p>
+          <AboutUsBtn />
+        </div>
+        <div>
+          <p>Log in</p>
+          <LoginBtn />
+        </div>
+        <div>
+          <p>Contact Us</p>
+          <ContactUsBtn />
+        </div>
+      </div>
+    );
+  }
+}
+
+class AboutUsPg extends React.Component {
+  render() {
+    return(
+      <h1>About Us</h1>
+    );
+  }
+}
+
+class ContactUsPg extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Contact Us</h1>
+        <p>Text</p>
+        <ContactUsBody />
+      </div>
+    );
+  }
+}
+
+// Buttons
+
+class HomeBtn extends React.Component {
+  render() {
+    return(
+      <button>Home</button>
+    );
+  }
+}
+
+class AboutUsBtn extends React.Component {
+  render() {
+    return(
+      <button>About Us</button>
+    );
+  }
+}
+
+class ContactUsBtn extends React.Component {
+  render() {
+    return(
+      <button>Contact Us</button>
+    );
+  }
+}
+
+class LoginBtn extends React.Component {
+  render() {
+    return(
+      <button>Login</button>
+    );
+  }
+}
+
+// Main App Components
+
+class NavBar extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Company</p>
+        <HomeBtn />
+        <AboutUsBtn />
+        <ContactUsBtn />
+        <LoginBtn />
+      </div>
+    );
+  }
+}
+
+class Body extends React.Component {
+  render() {
+    return(
+      <HomePg />
+      // <AboutUsPg />
+      // <ContactUsPg />
+    );
+  }
+}
 
 class App extends React.Component {
   render() {
@@ -9,7 +139,9 @@ class App extends React.Component {
       <div>
         <NavBar />
         <Body />
-        <footer></footer>
+        <footer>
+          <p>Footer</p>
+        </footer>
       </div>
     );
   }
@@ -21,8 +153,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
